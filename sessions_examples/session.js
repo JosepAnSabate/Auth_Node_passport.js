7b611ebdecc40aafd5f6d33ab4d6cf235a0c15d7
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-12-15 14:04:13 +0100

    session

---
+++ sessions_examples/session.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const MongoStore = require('connect-mongo')(session);
+
+const app = express();
+
+const dbString = 'mongodb://localhost:27017/tutorial_db';
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+const connection = mongoose.createConnection(dbString, dbOptions);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const sessionStore = new MongoStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+});
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,  
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 24 // Equals 1 day
+    }
+}));
+
+app.get('/', (req, res, next) => {
+    //console.log(req.session);
+    if (req.session.viewCount) {
+        req.session.viewCount = req.session.viewCount +1;
+    } else {
+        req.session.viewCount = 1;
+    }
+    res.send(`<h1>You have visited this page ${req.session.viewCount} times.</h1>`);
+})
+
+app.listen(3100);
